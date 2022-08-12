293bb18c338292a4632d5d7c4f5854b6707e834c
Yulia <yulia1201556@gmail.com>
2022-08-12 14:42:41 +0300

    Нахождение расстояния через метод

--- task21/Program.cs
+++ task21/Program.cs
@@ -4,7 +4,29 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Clear();
+// Console.Clear();
+
+// Console.WriteLine("Введите координаты для точки A: ");
+// Console.Write("Введите х: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координаты для точки B: ");
+// Console.Write("Введите х: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+
+// Console.WriteLine(Math.Round(result,2));
+
+
 
 Console.WriteLine("Введите координаты для точки A: ");
 Console.Write("Введите х: ");
@@ -22,6 +44,9 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите z: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+double Distance(int xc1, int yc1, int zc1, int xc2, int yc2, int zc2)
+{
+    return Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)),2 );
+}
 
-Console.WriteLine(Math.Round(result,2));
+Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}) == {Distance( x1,  y1,  z1,  x2,  y2,  z2)} ");

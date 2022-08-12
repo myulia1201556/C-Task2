24065a66aa10e8c4078d381b474d7bd73ed13c90
Yulia <yulia1201556@gmail.com>
2022-08-12 14:12:58 +0300

    Нахождение расстояния между заданными точками

---
+++ task21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21. Напишите программу, которая
+// 1. принимает на вход координаты двух точек 
+// 2. и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты для точки A: ");
+Console.Write("Введите х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты для точки B: ");
+Console.Write("Введите х: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+
+Console.WriteLine(Math.Round(result,2));

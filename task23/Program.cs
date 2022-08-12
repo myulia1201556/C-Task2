fb8fa94edcd3aa3d78df57a8be1100acbb23e1e0
Yulia <yulia1201556@gmail.com>
2022-08-12 15:47:59 +0300

    Число в кубе

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая 
+// 1. принимает на вход число (N) и
+// 2.  выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+ 
+ while (i <= num)
+ {
+    int cube = i*i*i;
+    Console.WriteLine($"{i} в 3 степени - {cube}");
+    i++;
+ }
